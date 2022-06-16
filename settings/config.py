6f3fc95941c5dc601d801b34339bcbb21843ba56
alone <83868709+samuraii-company@users.noreply.github.com>
2022-06-16 20:18:10 +0300

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 SECRET_TOKEN = os.getenv("SECRET_KEY", "super_secret_key")
 ALGORITHM = "HS256"
 
-DATABASE_HOST = os.getenv("DATABASE_HOST", "0.0.0.0")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "database")
 DATABASE_PORT = os.getenv("DATABASE_PORT", 5432)
 DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
 DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "postgres")
